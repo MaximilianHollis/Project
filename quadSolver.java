5e9bf477946305c05e5b066e133a6ecdb6c3790d
MaximilianHollis <50596827+MaximilianHollis@users.noreply.github.com>
2019-05-23 08:13:07 -0400

    Update quadSolver.java

--- quadSolver.java
+++ quadSolver.java
@@ -5,36 +5,34 @@ public class quadSolver {
 
 	public static void main(String[] args) {
 		System.out.println("Enter quadratic function");
+		System.out.println("Must be in ax^2+bx+c form");
+
 		Scanner input = new Scanner(System.in);
 		String quad = input.next();
-		
-		
-		
+
 		String s = quad;
 
 		int i=s.indexOf("x^2");
-		System.out.println(i);
 		int j=s.indexOf("x", i + 1);
-		System.out.println(j);
 		int k= j + 1;
 		
 		int length = quad.length();
 		
-		String a = quad.substring(0, i);
-		String b = quad.substring(i+4, j);
-		String c = quad.substring(j+2, length);
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+		String a1 = quad.substring(0, i);
+		String b1 = quad.substring(i+3, j);
+		String c1 = quad.substring(j+1, length);
+		
+		int a = Integer.parseInt(a1);
+		int b = Integer.parseInt(b1);
+		int c = Integer.parseInt(c1);
 
+		double root1 = ((b * -1) + Math.sqrt(Math.pow(b, 2) -(4 * a * c)))/(2*a);
+				
+		double root2 = ((b * -1) - Math.sqrt(Math.pow(b, 2) -(4 * a * c)))/(2*a);
 		
-		
-		
-		
-		
-		
-		
-		
+		System.out.println(root1);
+		System.out.println(root2); 
+	
 	}
 	
 	
