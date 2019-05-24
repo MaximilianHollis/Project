64559f7a0f003a10dded6ce795d6e0eee2d8339b
MaximilianHollis <50596827+MaximilianHollis@users.noreply.github.com>
2019-05-24 08:15:34 -0400

    Add files via upload

--- Tic_Tac_Toe.java
+++ Tic_Tac_Toe.java
@@ -1,26 +1,62 @@
 import java.util.Scanner; 
 
 public class Tic_Tac_Toe {
-	public static void main(String [] args) {
-		System.out.println("|-|-|-|");		
-		System.out.println("|-|-|-|");
-		System.out.println("|-|-|-|");
 
-		 int player = 1;
-		 Scanner obj1 = new Scanner(System.in);  // Create a Scanner object
-		 System.out.println( "player: " + player + " enter first move");
-		 int mv1 = obj1.nextInt();  // Procure user input
+
+	static String turn = "x";
+
+	static String[] board = {"-","-","-","-","-","-","-","-","-"};
+	
+	
+	
+	 static void myMethod() {
+		 
+		    System.out.println("Player: "+ turn + ", enter move \n[0][1][2]\n[3][4][5]\n[6][7][8]");
+
+			Scanner input = new Scanner(System.in);
+			String move = input.next();
+			int move1 = Integer.parseInt(move);
+			board[move1] = turn;
+			
+			System.out.print("[" + board[0] +"]");
+			System.out.print("[" + board[1] +"]");
+			System.out.println("[" + board[2] +"]");
+			
+			System.out.print("[" + board[3] +"]");
+			System.out.print("[" + board[4] +"]");
+			System.out.println("[" + board[5] +"]");
+			
+			System.out.print("[" + board[6] +"]");
+			System.out.print("[" + board[7] +"]");
+			System.out.print("[" + board[8] +"]");	
+			
+			if (turn.equals("x")) {
+				turn = "y";
+			}
+			else {
+				turn = "x";
+			}
+		    
+		    
+		    
+		  }
+	
+	 public static void main(String [] args) {
 		
-		 player = 2;
-		 Scanner obj2 = new Scanner(System.in); 
-		 System.out.println( "player: " + player + " enter first move");
-		 int mv2 = obj1.nextInt(); 
+		 String turn = "x";
+			
+		 myMethod();
+		 myMethod();
+		 myMethod();
+		
+		
+		
 		
 		
 		
 	}
 
-	
+
 	
 	
 	
