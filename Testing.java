95728351b013e224722f59acacafa7b4cb2952ef
MaximilianHollis <50596827+MaximilianHollis@users.noreply.github.com>
2019-05-29 08:12:16 -0400

    Add files via upload

--- Testing.java
+++ Testing.java
@@ -1,16 +1,65 @@
+import java.util.Scanner; 
+
 
 public class Testing {
 
-	public static void main(String[] args) {
-		
+	static void dowhileloop(){
 		int i = 1;
 		do {
 			System.out.println("Hello World. This is what i currently is: " + i);
 			i++;
+			
+		} while(i < 11);
+		}
+	
+	static void forloop(){
+		for(int i = 1; i < 11; i++) {
+			System.out.println("Hello World. This is what i currently is: " + i);
+			
+		} 
+		}
+	
+	static void whileloop(){
+		int i = 1;
+		 while(i < 11) {
+			System.out.println("Hello World. This is what i currently is: " + i);
+		    i++;
 
-		} while(i < 10);
+		}
+		}
+
+	
+	
+	
+	public static void main(String[] args) {
 		
+		System.out.println("select your favorite type of loop (dowhileloop, forloop, or whileloop.");
+		Scanner input = new Scanner(System.in);
+		String selection = input.next();
 		
+		if (selection.equals("dowhileloop")) {
+			dowhileloop();
+		}else {
+			
+			if (selection.equals("forloop")) {
+				forloop();
+			}else {
+				
+				if (selection.equals("forloop")) {
+					whileloop();
+				}else {
+					System.out.println("invalid selection");
+						main(args);
+				
+					
+				}
+			
+			}
+			
+	
+			
+		
+		}
 		
 		
 	}
