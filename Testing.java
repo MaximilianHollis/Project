394a4b9bcdbe0bb50000a42155b85d6c6171d65e
MaximilianHollis <50596827+MaximilianHollis@users.noreply.github.com>
2019-05-29 08:10:50 -0400

    Loops

---
+++ Testing.java
@@ -0,0 +1,17 @@
+
+public class Testing {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		do {
+			System.out.println("Hello World. This is what i currently is: " + i);
+			i++;
+
+		} while(i < 10);
+		
+		
+		
+		
+	}
+}
