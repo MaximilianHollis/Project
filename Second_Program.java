7e2420384dd9c58a90af0009011f99787f317565
MaximilianHollis <50596827+MaximilianHollis@users.noreply.github.com>
2019-05-29 07:48:09 -0400

    Delete Second_Program.java

--- Second_Program.java
+++
@@ -1,9 +0,0 @@
-
-public class Second_Program {
-	public static void main(String [] args)
-	{
-		System.out.println("My best friends:\n");
-		System.out.println("\tJeffrey Y.");
-		System.out.println("\tDaniel H.");
-	}
-}
